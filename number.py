c66e60d02d4d6bf7feef46b2c580d2716760db33
uwinezagloria <gloria7wineza.com>
2025-10-06 11:43:35 +0200

    math functions and operators

---
+++ number.py
@@ -0,0 +1,26 @@
+import math # importing math library so that you can work with all maths functions
+x=2
+y=1.2
+z=2+3j
+print(type(x)) # int
+print(type(y))# floar
+print(type(z)) #complex
+# operators
+sum=10+3
+product=10*3
+power=10 ** 3
+substract=10-3
+div=10/3 # you get also decimals
+division=10//3 # you get only integer without decimals
+mod=10%3
+print(sum)
+print(product)
+print(power)
+print(substract)
+print(div)
+print(division)
+print(mod)
+# working with numbers
+print(abs(-8)) # thi is absolute display 8 
+print(round(2.5))
+print(math.sqrt(2)) #working with math we imported to find square root
